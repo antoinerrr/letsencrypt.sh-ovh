030ec7870d4af4fb6d466b52629c5f79d9af7b76
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2018-04-29 21:44:35 +0200

    Increase sleeping time
    Most of the time, sleeping 5 seconds isn't enough.

--- manual-auth-hook.py
+++ manual-auth-hook.py
@@ -30,4 +30,4 @@ id_record = client.post('/domain/zone/%s/record' % basedomain,
                         target=token)
 print (str(id_record["id"]))
 client.post('/domain/zone/%s/refresh' % basedomain)
-time.sleep(5)
+time.sleep(10)
